d7ef281bda2bd0988b69922e969a0e10d9c248c9
fayching <435462399@163.com>
2014-09-20 14:36:45 +0800

    m

--- static/layout.js
+++ static/layout.js
@@ -1,7 +1,6 @@
 function write_header(){
     var template_header = '<div class="head-content">'+
         '<div class="title-area">'+
-            '<img src="http://frozenui.github.io/static/frozen-top.png" alt="frozen" />'+
             '<h1>'+
                 '<a href="/">Frozen</a>'+
             '</h1>'+                 
@@ -9,8 +8,8 @@ function write_header(){
       
       '<div class="nav-area">'+
           '<ol class="main-nav">'+
-              '<li><a href="/baseui">Css</a></li>'+
-              '<li><a href="/frozenjs">Javascript</a></li>'+
+              '<li><a href="/baseui">CSS</a></li>'+
+              '<li><a href="/frozenjs">JavaScript</a></li>'+
               '<li><a href="/docs/case.html">Animation case</a></li>'+
               '<li><a href="/docs/start.html">Getting started</a></li>'+
               '<li><a href="/docs/about.html">About</a></li>'+
@@ -22,9 +21,10 @@ function write_header(){
 }
 function write_footer(){
     var template_footer = '<div class="footer-content">'+
-    	'<div id="footer-us">'+
-    		'<p>© QQ会员前端开发组<br/>All rights reserved.</p>'+
-    	'</div>'+
+		'<a href="https://github.com/frozenui">GitHub</a> • '+
+        '<a href="http://lab.lepture.com/nico/">nico</a> • '+
+        '<a href="https://github.com/frozenui/frozenui/issues">Issues</a> • '+
+        '<a href="https://github.com/QQVIPTeam">QQVIP FD Team</a>'
     '</div>';
     document.write(template_footer);
 }
