0512354536475d94693220ee80f8d6289d28e7ae
fayching <435462399@163.com>
2015-06-12 17:25:58 +0800

    head

--- js/layout.js
+++ js/layout.js
@@ -9,12 +9,12 @@ $(function() {
         '<div class="nav-area">'+
             '<a class="nav-toggle"></a>'+
                 '<ol class="main-nav">'+
-                    '<li><a href="start.html">快速开始</a></li>'+
-                    '<li><a href="base.html">基础组件</a></li>'+
-                    '<li><a href="components.html">UI 组件</a></li>'+
+                    '<li><a href="/start.html">快速开始</a></li>'+
+                    '<li><a href="/base.html">基础组件</a></li>'+
+                    '<li><a href="/components.html">UI 组件</a></li>'+
                     '<li><a href="http://frozenui.github.io/frozenjs/">JS 插件</a></li>'+
-                    '<li><a href="case.html">动效库</a></li>'+
-                    '<li><a href="customize.html">下载</a></li>'+
+                    '<li><a href="/case.html">动效库</a></li>'+
+                    '<li><a href="/customize.html">下载</a></li>'+
                     '<li><a href="/frozenui/demo/index.html">Demo</a></li>'+
                     '<li><a href="https://github.com/frozenui/frozenui">GitHub</a></li>'+
                 '</ol>'+
