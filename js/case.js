e91af13fcedb17ea23247130c878a5bf8dd07558
Hahn <hahn.chu91@gmail.com>
2017-05-26 11:22:01 +0800

    Update case.js

--- js/case.js
+++ js/case.js
@@ -79,7 +79,7 @@
             for(var i=0; i<j; i++){
             var imgname = datalist[i].thumb.split(",");
             if(datalist[i].imgPath == undefined){
-              datalist[i].imgPath ='http://119.29.8.64/frozen_case/Public/';
+              datalist[i].imgPath ='http://119.29.8.64/vipstyle/web/case/img/';
             }
             $(".caselist").append('<div class="item"><div class="thumb"><a class="case-link" target="_blank" href="'+datalist[i].url+'"><img src="'+datalist[i].imgPath+imgname[0]+'.jpg" alt=""><img src="'+datalist[i].imgPath+imgname[1]+'.jpg" alt=""><img src="'+datalist[i].imgPath+imgname[2]+'.jpg" alt=""><div class="qr"></div><div class="mask"></div></a></div><div class="meta"><div class="title"><a href="'+datalist[i].url+'" target="_blank">'+datalist[i].title+'</a><span class="author"><a target="_blank" href="'+datalist[i].homepage+'"><i></i>'+datalist[i].name+'</a></span></div><div class="desc">'+datalist[i].description+'</div></div></div>');
         }
